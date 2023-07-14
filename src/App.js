ce807eccbf9b66cf3bd4fe9c7fad366950558390
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2023-07-14 18:04:26 -0400

    tercera version: Se envia nombre

--- src/App.js
+++ src/App.js
@@ -1,1 +1,6 @@
-alert("Hola desde Javascript");
+const nombre = document.querySelector("#nombre-input");
+const form = document.querySelector("#saludador-form");
+
+form.addEventListener("submit", (event) => {
+    alert("Hola " + nombre.value);
+})
