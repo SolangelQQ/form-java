713f1ea6662d264e023ee3a3e4b8fe9af1686f36
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2023-07-14 19:01:10 -0400

    Cuarta version: Saludar tomando en cuenta edad

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 const nombre = document.querySelector("#nombre-input");
 const form = document.querySelector("#saludador-form");
+const edad = document.querySelector("#edad-input");
+
+function saludarSegunEdad(edad){
+    return (edad>=11 && edad<=25) ? "joven " : (edad>=26) ? "senor(a) " : " ";
+}
 
 form.addEventListener("submit", (event) => {
-    alert("Hola " + nombre.value);
-})
+    let saludarPorEdad = saludarSegunEdad(edad.value);
+    alert("Hola "+ saludarPorEdad + nombre.value);
+});
