b2f2f44a01fb16f30c17a5751c22c77d529e08c5
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2023-07-15 18:46:36 -0400

    Octava version: Saludar tomando en cuenta el idioma(mejorado v2)

--- src/App.js
+++ src/App.js
@@ -9,13 +9,31 @@ function saludarSegunEdad(edad){
     return (edad>=11 && edad<=25) ? "joven" : (edad>=26) ? "Senor(a)" : " ";
 }
 
-function saludarSegunGenero(genero, tipo){
+function saludarSegunGenero(genero, tipo, idioma){
+    let diccionario_joven = new Map([
+        ['espaniol', ['senorita ', 'joven ']],
+        ['ingles', ['miss ', 'young ']],
+        ['aleman', ['fräulein ', 'junge ']],
+        ['ruso', ['Мисс ', 'молодой ']],
+        ['chino', ['小姐 ', '年轻 ']],
+        ['frances', ['mademoiselle ', 'jeune ']]
+    ]);
+
+    let diccionario_adulto = new Map([
+        ['espaniol', ['senora ', 'senor ']],
+        ['ingles', ['madam ', 'mister ']],
+        ['aleman', ['frau ', 'herr ']],
+        ['ruso', ['Г-жа ', 'Г-н ']],
+        ['chino', ['女士 ', '先生 ']],
+        ['frances', ['Mme ', 'monsieur ']]
+    ]);
+
     switch(tipo){ 
         case "joven": 
-            return (genero == "mujer") ? "senorita " : "joven ";
+            return (genero == "mujer" ) ? diccionario_joven.get(idioma)[0] : diccionario_joven.get(idioma)[1];
             break;
         case "Senor(a)":
-            return (genero == "mujer") ? "senora " : "senor ";
+            return (genero == "mujer") ? diccionario_adulto.get(idioma)[0] : diccionario_adulto.get(idioma)[1];
             break;
         case " ":
             return " ";
@@ -31,36 +49,36 @@ function saludarSegunHora(hora){
 function saludarSegunIdioma(idioma, horario){
     switch(horario){
         case "buen dia ":
-            return (idioma == "espaniol") ? "buen dia " : 
-            (idioma == "ingles") ? "good morning " :
-            (idioma == "aleman")? "guten Morgen " : 
-            (idioma == "ruso")? "доброе утро ":
-            (idioma == "chino")? "早上好 " :
-            (idioma == "frances")? "bonjour  " : "" ;
+            return (idioma == "espaniol") ? "Hola, buen dia " : 
+            (idioma == "ingles") ? "Hello, good morning " :
+            (idioma == "aleman") ? "Hallo, guten Morgen " : 
+            (idioma == "ruso") ? "здравствуйте, доброе утро ":
+            (idioma == "chino") ? "您好, 早上好 " :
+            (idioma == "frances") ? "Bonjour  " : "" ;
             break;
         case "buenas tardes ":
-            return (idioma == "espaniol") ? "buenas tardes " : 
-            (idioma == "ingles") ? "good afternoon " :
-            (idioma == "aleman")? "guten Tag " : 
-            (idioma == "ruso")? "Добрый день ":
-            (idioma == "chino")? "下午好 " :
-            (idioma == "frances")? "bonjour à tous  " : "" ;
+            return (idioma == "espaniol") ? "Hola, buenas tardes " : 
+            (idioma == "ingles") ? "Hello, good afternoon " :
+            (idioma == "aleman") ? "Hallo, guten Tag " : 
+            (idioma == "ruso") ? "здравствуйте, Добрый день ":
+            (idioma == "chino") ? "您好,下午好 " :
+            (idioma == "frances") ? "Bonjour à tous  " : "" ;
             break;
         case "buenas noches ":
-            return (idioma == "espaniol") ? "buenas noches " : 
-            (idioma == "ingles") ? "good night " :
-            (idioma == "aleman")? "guten Abend " : 
-            (idioma == "ruso")? "добрый вечер ":
-            (idioma == "chino")? "晚上好 " :
-            (idioma == "frances")? "bonne soirée  " : "" ;
+            return (idioma == "espaniol") ? "Hola, buenas noches " : 
+            (idioma == "ingles") ? "Hello, good night " :
+            (idioma == "aleman") ? "Hallo, guten Abend " : 
+            (idioma == "ruso") ? "здравствуйте, добрый вечер ":
+            (idioma == "chino") ? "您好, 晚上好 " :
+            (idioma == "frances") ? "Bonne soirée  " : "" ;
             break;      
     }
 }
 
 form.addEventListener("submit", (event) => {
     let saludarPorEdad = saludarSegunEdad(edad.value);
-    let saludarPorGenero = saludarSegunGenero(genero.value, saludarPorEdad);
+    let saludarPorGenero = saludarSegunGenero(genero.value, saludarPorEdad, idioma.value);
     let saludarPorHora = saludarSegunHora(fecha.getHours());
     let saludarPorIdioma = saludarSegunIdioma(idioma.value, saludarPorHora);
-    alert("Hola, "+ saludarPorIdioma + saludarPorGenero + nombre.value);
+    alert(saludarPorIdioma + saludarPorGenero + nombre.value);
 });
