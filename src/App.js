367cee2fa767b9d26381c7f3f53eb780357bdb56
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2023-07-14 22:46:24 -0400

    Quinta version: Saludar tomando en cuenta el genero

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 const nombre = document.querySelector("#nombre-input");
 const form = document.querySelector("#saludador-form");
 const edad = document.querySelector("#edad-input");
+const genero = document.querySelector("#genero-input");
 
 function saludarSegunEdad(edad){
-    return (edad>=11 && edad<=25) ? "joven " : (edad>=26) ? "senor(a) " : " ";
+    return (edad>=11 && edad<=25) ? "joven" : (edad>=26) ? "Senor(a)" : " ";
+}
+
+function saludarSegunGenero(genero, tipo){
+    switch(tipo){ 
+        case "joven": 
+            return (genero == "mujer") ? "senorita " : "joven ";
+            break;
+        case "Senor(a)":
+            return (genero == "mujer") ? "senora " : "senor ";
+            break;
+        case " ":
+            return " ";
+            break;
+    }
 }
 
 form.addEventListener("submit", (event) => {
     let saludarPorEdad = saludarSegunEdad(edad.value);
-    alert("Hola "+ saludarPorEdad + nombre.value);
+    let saludarPorGenero = saludarSegunGenero(genero.value, saludarPorEdad);
+    alert("Hola "+ saludarPorGenero + nombre.value);
 });
